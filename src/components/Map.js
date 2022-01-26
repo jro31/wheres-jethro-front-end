c336dfaa2ee23fab66c891e4672da3b7770c2e33
Jethro Williams <jethrowilliams@outlook.com>
2022-01-26 17:01:06 +0000

    Display some emoji markers

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 
 function Map() {
   const [viewport, setViewport] = useState({
     width: '100vw',
-    height: 400,
+    height: '100vh',
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,
@@ -17,7 +17,14 @@ function Map() {
       mapStyle='mapbox://styles/mapbox/satellite-streets-v11'
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-    />
+    >
+      <Marker latitude={37.7577} longitude={-122.4376}>
+        🖕
+      </Marker>
+      <Marker latitude={0} longitude={0}>
+        🤿
+      </Marker>
+    </ReactMapGL>
   );
 }
 
