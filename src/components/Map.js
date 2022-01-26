251750a92c8f457a9407330ad22b29b66723705f
Jethro Williams <jethrowilliams@outlook.com>
2022-01-26 17:37:34 +0000

    Display markers with pop-ups

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,25 @@
 import * as React from 'react';
 import { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 
-function Map() {
+const DUMMY_DATA = [
+  {
+    latitude: 37.7577,
+    longitude: -122.4376,
+    name: 'San Francisco',
+    description: 'Bunch of smug fuckers',
+    icon: '🖕',
+  },
+  {
+    latitude: 0,
+    longitude: 0,
+    name: 'Middle of the sea',
+    description: 'Good for swimming',
+    icon: '🤿',
+  },
+];
+
+const Map = () => {
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -10,6 +27,11 @@ function Map() {
     longitude: -122.4376,
     zoom: 8,
   });
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  const markerClickHandler = location => {
+    setSelectedMarker(location);
+  };
 
   return (
     <ReactMapGL
@@ -18,14 +40,29 @@ function Map() {
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
     >
-      <Marker latitude={37.7577} longitude={-122.4376}>
-        🖕
-      </Marker>
-      <Marker latitude={0} longitude={0}>
-        🤿
-      </Marker>
+      {DUMMY_DATA.map(location => (
+        <Marker
+          key={location.latitude}
+          latitude={location.latitude}
+          longitude={location.longitude}
+          onClick={() => markerClickHandler(location)}
+        >
+          {location.icon}
+        </Marker>
+      ))}
+
+      {selectedMarker && (
+        <Popup
+          latitude={selectedMarker.latitude}
+          longitude={selectedMarker.longitude}
+          onClose={() => setSelectedMarker(null)}
+        >
+          <h2>{selectedMarker.name}</h2>
+          <p>{selectedMarker.description}</p>
+        </Popup>
+      )}
     </ReactMapGL>
   );
-}
+};
 
 export default Map;
