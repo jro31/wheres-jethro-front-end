8976e94847f5ae2c3251d3338b541016356ab4ea
Jethro Williams <jethrowilliams@outlook.com>
2022-01-31 15:39:49 +0000

    Add route to check-in page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import AddCheckIn from './routes/AddCheckIn';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='check-in' element={<AddCheckIn />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/routes/AddCheckIn.js
@@ -0,0 +1,5 @@
+const AddCheckIn = () => {
+  return <h1>ADD CHECK IN PAGE</h1>;
+};
+
+export default AddCheckIn;
