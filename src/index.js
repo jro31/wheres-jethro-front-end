58d8dc89daa84776e449584bdf52124e14b3a7a2
Jethro Williams <jethrowilliams@outlook.com>
2022-02-04 13:48:51 +0000

    Move router to app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import AddCheckIn from './routes/AddCheckIn';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='check-in' element={<AddCheckIn />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
