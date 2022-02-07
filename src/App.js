c06c2ecde61d5bdf7c3ee7f1cdaa020d095fa2a4
Jethro Williams <jethrowilliams@outlook.com>
2022-02-07 19:11:10 +0000

    Remove lazy-loading

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 // TODO - Update readme
 
-import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import MapPage from './routes/MapPage';
-
-const AddCheckIn = React.lazy(() => import('./routes/AddCheckIn'));
+import AddCheckIn from './routes/AddCheckIn';
 
 const App = () => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <Routes>
-        <Route path='/' element={<MapPage />} />
-        <Route path='check-in' element={<AddCheckIn />} />
-      </Routes>
-    </Suspense>
+    <Routes>
+      <Route path='/' element={<MapPage />} />
+      <Route path='check-in' element={<AddCheckIn />} />
+    </Routes>
   );
 };
 
